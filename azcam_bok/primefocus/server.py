dff127ea8a3851b2721a0771eb2f35b764874046
Michael Lesser <mlesser@arizona.edu>
2020-11-22 15:51:35 -0700

    cleanup

--- azcam_bok/primefocus/server.py
+++ azcam_bok/primefocus/server.py
@@ -109,8 +109,8 @@ azcam.db.parfile = parfile
 # enable logging
 # ****************************************************************
 tt = datetime.datetime.strftime(datetime.datetime.now(), "%d%b%y_%H%M%S")
-azcam.db.logfile = os.path.join(azcam.db.datafolder, "logs", f"server_{tt}.log")
-azcam.logging.start_logging(azcam.db.logfile, "123")
+azcam.db.logger.logfile = os.path.join(azcam.db.datafolder, "logs", f"server_{tt}.log")
+azcam.db.logger.start_logging(azcam.db.logger.logfile, "123")
 
 azcam.log("Configuring for 90prime")
 
